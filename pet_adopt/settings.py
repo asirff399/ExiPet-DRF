2412255fe523fa028d77979999626258c5d85f48
asirff399 <aintisar48@gmail.com>
2024-10-04 21:58:33 +0600

    ...

--- pet_adopt/settings.py
+++ pet_adopt/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
-import dj_database_url
 import environ
 env = environ.Env()
 environ.Env.read_env()
