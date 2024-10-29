aa78d042cabd213684439f43fc260ea0ed0372a1
asirff399 <aintisar48@gmail.com>
2024-10-29 15:35:18 +0600

    [Fixed] - payment link

--- transaction/urls.py
+++ transaction/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path,include
-from rest_framework.routers import DefaultRouter
+from .views import InitiatePaymentView,payment_cancel,payment_fail,payment_success
 # from .views import 
 
 urlpatterns = [
-    # path('payment/initiate/<int:post_id>', InitiatePaymentView.as_view(), name='initiate_payment'),
-    # path('payment/success/<int:user_id>/<int:post_id>/<int:amount>/<tran_id>', payment_success, name='payment_success'),
-    # path('payment/fail/', payment_fail, name='payment_fail'),
-    # path('payment/cancel/', payment_cancel, name='payment_cancel'),
+    path('payment/initiate/<int:post_id>', InitiatePaymentView.as_view(), name='initiate_payment'),
+    path('payment/success/<int:user_id>/<int:post_id>/<int:amount>/<tran_id>', payment_success, name='payment_success'),
+    path('payment/fail/', payment_fail, name='payment_fail'),
+    path('payment/cancel/', payment_cancel, name='payment_cancel'),
 ] 
