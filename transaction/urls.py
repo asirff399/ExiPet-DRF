49da507f617bfa2002fab193cf6c332d0c75e129
asirff399 <aintisar48@gmail.com>
2024-10-29 15:40:14 +0600

    [Fixed] - payment link

--- transaction/urls.py
+++ transaction/urls.py
@@ -3,8 +3,8 @@ from .views import InitiatePaymentView,payment_cancel,payment_fail,payment_succe
 # from .views import 
 
 urlpatterns = [
-    path('payment/initiate/<int:post_id>', InitiatePaymentView.as_view(), name='initiate_payment'),
-    path('payment/success/<int:user_id>/<int:post_id>/<int:amount>/<tran_id>', payment_success, name='payment_success'),
+    path('payment/initiate/<int:pet_id>', InitiatePaymentView.as_view(), name='initiate_payment'),
+    path('payment/success/<int:user_id>/<int:pet_id>/<int:amount>/<tran_id>', payment_success, name='payment_success'),
     path('payment/fail/', payment_fail, name='payment_fail'),
     path('payment/cancel/', payment_cancel, name='payment_cancel'),
 ] 
